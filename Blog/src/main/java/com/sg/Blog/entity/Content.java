72d4c20558b6d16eab1af94342f59907ab984dc9
Mohamed Ahmed <mohamed@Mohameds-MacBook-Air.local>
2020-04-06 15:08:55 -0500

    Blog Project

---
+++ Blog/src/main/java/com/sg/Blog/entity/Content.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.Blog.entity;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ *
+ * @author mohamed
+ */
+public class Content {
+    private int id;
+    private String title;
+    private String body;
+    private boolean approved;
+    private User user;
+    private Set<Tags> hashtags;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<Tags> getHashtags() {
+        return hashtags;
+    }
+
+    public void setHashtags(Set<Tags> hashtags) {
+        this.hashtags = hashtags;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.id;
+        hash = 67 * hash + Objects.hashCode(this.title);
+        hash = 67 * hash + Objects.hashCode(this.body);
+        hash = 67 * hash + (this.approved ? 1 : 0);
+        hash = 67 * hash + Objects.hashCode(this.user);
+        hash = 67 * hash + Objects.hashCode(this.hashtags);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Content other = (Content) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.approved != other.approved) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.body, other.body)) {
+            return false;
+        }
+        if (!Objects.equals(this.user, other.user)) {
+            return false;
+        }
+        if (!Objects.equals(this.hashtags, other.hashtags)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
