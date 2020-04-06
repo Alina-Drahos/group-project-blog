a6593d653727d83d2ee753def0acad6e82900fe2
Carlos Chavez <cycvez@gmail.com>
2020-04-06 16:56:02 -0500

    db added

---
+++ BlogDB.sql
@@ -0,0 +1,49 @@
+drop database if exists Blog;
+
+create database Blog;
+
+use Blog;
+
+create table Users(
+	Id int primary key auto_increment,
+    username varchar(45) not null,
+    `password` varchar(45) not null,
+    enabled tinyint not null
+);
+
+create table Content(
+	Id int primary key auto_increment,
+    userId int not null,
+    body varchar(255) not null,
+    approved tinyint not null
+);
+
+create table Role(
+	Id int primary key auto_increment,
+    userRole varchar(45) not null
+);
+
+create table Tag(
+	Id int primary key auto_increment,
+    hashTag varchar(45) not null
+);
+
+create table Content_Tag(
+	primary key (Id_Content, Id_Tag),
+	Id_Content int not null,
+    Id_Tag int not null,
+	foreign key (Id_Content) references Content(Id),
+    foreign key (Id_Tag) references Tag(Id)
+);
+
+create table User_Role(
+	primary key (Id_Users, Id_Role),
+	Id_Users int not null,
+    Id_Role int not null,
+	foreign key (Id_Users) references Users(Id),
+    foreign key (Id_Role) references Role(Id)
+);
+
+
+
+
