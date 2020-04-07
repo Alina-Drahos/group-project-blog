48b3e3710963b0b5c1de5c094b4b741471102cab
Alina Drahos <alinchen1066@live.de>
2020-04-07 13:55:38 -0500

    UserController+UserDao+RoleDao

--- Blog/src/main/java/com/sg/Blog/Controller/AdminController.java
+++ Blog/src/main/java/com/sg/Blog/Controller/AdminController.java
@@ -15,4 +15,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class AdminController {
     
+    
+    
 }
---
+++ Blog/src/main/java/com/sg/Blog/Controller/UserController.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.Blog.Controller;
+
+import com.sg.Blog.dao.ContentDao;
+import com.sg.Blog.dao.RoleDao;
+import com.sg.Blog.dao.TagDao;
+import com.sg.Blog.dao.UserDao;
+import com.sg.Blog.entity.Role;
+import com.sg.Blog.entity.User;
+import java.util.ArrayList;
+import java.util.Base64.Encoder;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author alinc
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    UserDao userDao;
+
+    @Autowired
+    RoleDao roleDao;
+
+    @Autowired
+    ContentDao contentDao;
+
+    @Autowired
+    TagDao tagDao;
+
+    @GetMapping("users")
+    public String displayUsers(Model model) {
+        List<User> users = userDao.getAllUsers();
+        List<Role> userRoles = roleDao.getAllRoles();
+        model.addAttribute("users", users);
+        model.addAttribute("userRoles", userRoles);
+        model.addAttribute("user", new User());
+        return "users";
+
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@Valid User user, BindingResult result, HttpServletRequest request, Model model) {
+        String[] roleIds = request.getParameterValues("roleId");
+        String password = request.getParameter("password");
+
+        Set<Role> userRoles = new HashSet<>();
+        if (roleIds != null) {
+            for (String roleId : roleIds) {
+                userRoles.add(roleDao.getRoleById(Integer.parseInt(roleId)));
+            }
+        } else {
+            FieldError error = new FieldError("user", "userRoles", "Must include one role");
+            result.addError(error);
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDao.getAllUsers());
+            model.addAttribute("roles", roleDao.getAllRoles());
+            return "/admin";
+        }
+
+        user.setRoles(userRoles);
+        user.setPassword(encoder.encode(password));
+        userDao.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("deleteUser")
+    public String deleteUSer(HttpServletRequest request) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        userDao.deleteUser(id);
+        return "redirect:/admin";
+
+    }
+
+    @GetMapping("editUser")
+    public String editUser(HttpServletRequest request, Model model) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        User user = userDao.getUserById(id);
+        user.setRoles(roleDao.getAllRolesforUser(user));
+        List<Role> userRoles = roleDao.getAllRoles();
+
+        model.addAttribute("user", user);
+        model.addAttribute("userRoles", userRoles);
+        return "user";
+
+    }
+
+    @PostMapping("editUser")
+    public String performEditUser(@Valid User user, BindingResult result, HttpServletRequest request,
+            Model model) {
+
+        String[] roleIds = request.getParameterValues("roleId");
+         String password = request.getParameter("password");
+
+        Set<Role> userRoles = new HashSet<>();
+        if (roleIds != null) {
+            for (String roleId : roleIds) {
+                userRoles.add(roleDao.getRoleById(Integer.parseInt(roleId)));
+            }
+        } else {
+            FieldError error = new FieldError("user", "userRoles", "Must include one role");
+            result.addError(error);
+        }
+
+        user.setRoles(userRoles);
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDao.getAllUsers());
+            model.addAttribute("roles", roleDao.getAllRoles());
+            return "/admin";
+        }
+
+        user.setPassword(encoder.encode(password));
+        userDao.addUser(user);
+        return "redirect:/admin";
+
+    }
+
+}
